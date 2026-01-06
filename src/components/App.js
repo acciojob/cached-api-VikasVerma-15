c726b16aef84546231fcd8214685974619b4c9c0
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2026-01-06 15:57:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useMemo, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const apiUrl = "https://jsonplaceholder.typicode.com/posts";
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+        setLoading(false);
+      });
+  }, [apiUrl]);
+
+  // cache API response
+  const cachedData = useMemo(() => data, [data]);
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <p>{cachedData.length} items fetched</p>
+      )}
+    </div>
+  );
+};
+
+export default App;
